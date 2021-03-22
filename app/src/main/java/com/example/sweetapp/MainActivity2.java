0a29c38e230b7e7c1c84f91494c35c611e364895
ahmad19678 <38799135+ahmad19678@users.noreply.github.com>
2021-03-22 20:57:02 +0200

    Test

--- app/src/main/java/com/example/sweetapp/MainActivity2.java
+++ app/src/main/java/com/example/sweetapp/MainActivity2.java
@@ -3,12 +3,16 @@ package com.example.sweetapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        text = findViewById(R.id.text);
+
     }
 }
