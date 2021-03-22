a99f6a60647a5ba7d2a776f0f429e8ab0c36aca4
Bad Man <ahmed_moon99@hotmail.com>
2021-03-22 21:36:51 +0200

    Welcom

--- app/src/main/java/com/example/sweetapp/MainActivity2.java
+++ app/src/main/java/com/example/sweetapp/MainActivity2.java
@@ -3,7 +3,9 @@ package com.example.sweetapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -13,6 +15,12 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         text = findViewById(R.id.text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "xXHXXXXxxxellodddddddddd", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
