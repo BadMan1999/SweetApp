6b9e74f8c1e24846c338c079df2b53b713fde878
ahmad19678 <38799135+ahmad19678@users.noreply.github.com>
2021-03-28 00:14:48 +0300

    slide menu

--- app/src/main/java/com/example/sweetapp/Activity/ChaletOwnerMainActivity.java
+++ app/src/main/java/com/example/sweetapp/Activity/ChaletOwnerMainActivity.java
@@ -1,16 +1,84 @@
 package com.example.sweetapp.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.sweetapp.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChaletOwnerMainActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    private Toolbar mToolbar;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chalet_owner_main);
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        drawerLayout = findViewById(R.id.drowerlayout);
+        navigationView = findViewById(R.id.navigationview);
+        mAuth = FirebaseAuth.getInstance();
+        navigationView.bringToFront();
+        ActionBarDrawerToggle dToggle = new ActionBarDrawerToggle(this,drawerLayout,mToolbar,R.string.navigation_open,R.string.navigation_close);
+        drawerLayout.addDrawerListener(dToggle);
+        dToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            sendUserToChooseTypeActivity();
+        }
+    }
+    private void sendUserToChooseTypeActivity() {
+        Intent regIntent = new Intent(ChaletOwnerMainActivity.this, ChooseTypeActivity.class);
+        startActivity(regIntent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.main_account_mangment:
+                break;
+            case R.id.main_chalet_list:
+
+                break;
+            case R.id.main_order_list:
+
+                break;
+            case R.id.main_logout_option:
+                mAuth.signOut();
+                sendUserToChooseTypeActivity();
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+
     }
 }
--- app/src/main/java/com/example/sweetapp/Activity/ChooseTypeActivity.java
+++ app/src/main/java/com/example/sweetapp/Activity/ChooseTypeActivity.java
@@ -8,9 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.sweetapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChooseTypeActivity extends AppCompatActivity {
     Button btn_Chalet_Owner,btn_tenant;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,7 @@ public class ChooseTypeActivity extends AppCompatActivity {
 
         btn_Chalet_Owner=findViewById(R.id.btn_Chalet_Owner);
         btn_tenant=findViewById(R.id.btn_tenant);
+        mAuth =FirebaseAuth.getInstance();
 
 
         btn_Chalet_Owner.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +44,17 @@ public class ChooseTypeActivity extends AppCompatActivity {
             }
         });
 
+    }
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            sendUserToActivety();
+        }
+    }
+
+    private void sendUserToActivety() {
+        Intent regIntent = new Intent(ChooseTypeActivity.this, ChaletOwnerMainActivity.class);
+        startActivity(regIntent);
     }
 }
